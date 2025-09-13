170f654e17da70bf943c2f18f1a1fb8b51fc3d37
Chris Goodman <cgoody27@live.com>
2025-09-12 21:30:17 -0500

    Add project files.

---
+++ Business/Commands/UpdatePerson.cs
@@ -0,0 +1,79 @@
+﻿using MediatR;
+using MediatR.Pipeline;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+using StargateAPI.Business.Data;
+using StargateAPI.Controllers;
+
+namespace StargateAPI.Business.Commands
+{
+    public class UpdatePerson : IRequest<UpdatePersonResult>
+    {
+        public required int Id { get; set; }
+        public required string Name { get; set; } = string.Empty;
+        public virtual AstronautDetail? AstronautDetail { get; set; }
+        public virtual ICollection<AstronautDuty> AstronautDuties { get; set; } = new HashSet<AstronautDuty>();
+    }
+
+    public class UpdatePersonPreProcessor : IRequestPreProcessor<UpdatePerson>
+    {
+        private readonly StargateContext _context;
+        public UpdatePersonPreProcessor(StargateContext context)
+        {
+            _context = context;
+        }
+        public Task Process(UpdatePerson request, CancellationToken cancellationToken)
+        {
+            var person = _context.People.AsNoTracking().FirstOrDefault(z => z.Name == request.Name);
+
+            if (person is null) {
+                throw new BadHttpRequestException("Bad Request");
+            };
+
+            return Task.CompletedTask;
+        }
+    }
+
+    public class UpdatePersonHandler : IRequestHandler<UpdatePerson, UpdatePersonResult>
+    {
+        private readonly StargateContext _context;
+
+        public UpdatePersonHandler(StargateContext context)
+        {
+            _context = context;
+        }
+        public async Task<UpdatePersonResult> Handle(UpdatePerson request, CancellationToken cancellationToken)
+        {
+            var person = await _context.People.FirstOrDefaultAsync(z => z.Id == request.Id, cancellationToken);
+
+            if (person is null)
+            {
+                throw new Exception($"{request.Name} not found");
+            }
+
+            person.Name = request.Name;
+            person.AstronautDetail = request.AstronautDetail;
+            person.AstronautDuties = request.AstronautDuties;
+
+            _context.People.Update(person);
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return new UpdatePersonResult()
+            {
+                Id = person.Id,
+                Name = person.Name,
+                AstronautDetail = person.AstronautDetail,
+                AstronautDuties = person.AstronautDuties
+            };
+        }
+    }
+
+    public class UpdatePersonResult : BaseResponse
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public virtual AstronautDetail? AstronautDetail { get; set; }
+        public virtual ICollection<AstronautDuty> AstronautDuties { get; set; } = new HashSet<AstronautDuty>();
+    }
+}
---
+++ Controllers/PersonController.cs
@@ -0,0 +1,123 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using StargateAPI.Business.Commands;
+using StargateAPI.Business.Data;
+using StargateAPI.Business.Queries;
+using System.Net;
+using System.Text.Json;
+
+namespace StargateAPI.Controllers
+{
+   
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public PersonController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("")]
+        public async Task<IActionResult> GetPeople()
+        {
+            try
+            {
+                var result = await _mediator.Send(new GetPeople()
+                {
+
+                });
+
+                return this.GetResponse(result);
+            }
+            catch (Exception ex)
+            {
+                return this.GetResponse(new BaseResponse()
+                {
+                    Message = ex.Message,
+                    Success = false,
+                    ResponseCode = (int)HttpStatusCode.InternalServerError
+                });
+            }
+        }
+
+        [HttpGet("{name}")]
+        public async Task<IActionResult> GetPersonByName(string name)
+        {
+            try
+            {
+                var result = await _mediator.Send(new GetPersonByName()
+                {
+                    Name = name
+                });
+
+                return this.GetResponse(result);
+            }
+            catch (Exception ex)
+            {
+                return this.GetResponse(new BaseResponse()
+                {
+                    Message = ex.Message,
+                    Success = false,
+                    ResponseCode = (int)HttpStatusCode.InternalServerError
+                });
+            }
+        }
+
+        [HttpPut("update")]  
+        public async Task<IActionResult> UpdatePerson([FromBody] Person person)
+        {
+            if (person == null)
+                throw new BadHttpRequestException("Bad Request - No person data provided");
+
+            try
+            {
+                //var person = JsonSerializer.Deserialize<UpdatePerson>(personJson);
+
+                var result = await _mediator.Send(new UpdatePerson()
+                {
+                    Id = person!.Id,
+                    Name = person!.Name,
+                    AstronautDetail = person?.AstronautDetail,
+                    AstronautDuties = person?.AstronautDuties ?? []
+                });
+
+                return this.GetResponse(result);
+            }
+            catch (Exception ex)
+            {
+                return this.GetResponse(new BaseResponse()
+                {
+                    Message = ex.Message,
+                    Success = false,
+                    ResponseCode = (int)HttpStatusCode.InternalServerError
+                });
+            }
+        }
+
+        [HttpPost("create")] // Added a unique route to resolve route conflict  
+        public async Task<IActionResult> CreatePerson([FromBody] string name)
+        {
+            try
+            {
+                var result = await _mediator.Send(new CreatePerson()
+                {
+                    Name = name
+                });
+
+                return this.GetResponse(result);
+            }
+            catch (Exception ex)
+            {
+                return this.GetResponse(new BaseResponse()
+                {
+                    Message = ex.Message,
+                    Success = false,
+                    ResponseCode = (int)HttpStatusCode.InternalServerError
+                });
+            }
+
+        }
+    }
+}
